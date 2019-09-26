3c11584d0f31280058e39ca6cd765f39bd07d044
Mgdolen zabad <mgdolen.zabad@gmail.com>
2019-09-26 00:17:15 -0400

    refacturing the comments removed the one from treehouse and add my comments

--- js/script.js
+++ js/script.js
@@ -3,40 +3,16 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
    
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
+//variables to get the student list from the document and set the student want to show in one page 
 const studentList = document.querySelectorAll('.student-item');
 const itemsPerPage = 10;
 
 
 
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+//hide all of the student in the list except for the ten we want to show
 
 function showPage (list, page) {
    const startIndex = (page * itemsPerPage) - itemsPerPage;
@@ -52,10 +28,8 @@ function showPage (list, page) {
 }
 
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+
+//dynamically create the pagination list and append it to the body and add functionality to the pagination buttons
 
 function appendPageLinks(list) {
    const container = document.querySelector('.page');
@@ -95,10 +69,9 @@ function appendPageLinks(list) {
 
 
 
-
+//calling the functions 
 showPage(studentList, 1);
 appendPageLinks(studentList);
 
 
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
