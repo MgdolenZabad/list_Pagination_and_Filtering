9c201e19204b1a61cab205dedfe62d471556da11
Mgdolen zabad <mgdolen.zabad@gmail.com>
2019-10-01 22:46:27 -0400

    creatting the search bar and adding its functionallity

--- js/script.js
+++ js/script.js
@@ -28,16 +28,16 @@ function showPage (list, page) {
 }
 
 
+const container = document.querySelector('.page');
+   const div = document.createElement('div');
+   div.className = ('pagination');
+   container.appendChild(div);
+
 
 //dynamically create the pagination list and append it to the body and add functionality to the pagination buttons
 
 function appendPageLinks(list) {
-   const container = document.querySelector('.page');
-   const div = document.createElement('div');
-   div.className = ('pagination');
-   container.appendChild(div);
-  
-
+   div.innerHTML='';
    const numberOfPages = Math.floor(list.length / itemsPerPage);
    const ul = document.createElement('ul');
    for (let i = 0; i < numberOfPages; i+= 1) {
@@ -51,7 +51,7 @@ function appendPageLinks(list) {
 
       ul.firstElementChild.firstElementChild.className = "active" ;
 
-
+      //adding event listener on the pagination links 
       a.addEventListener('click', (e) => {         
          for(let i=0 ; i < ul.children.length; i++){
             ul.children[i].firstElementChild.className = '';
@@ -61,17 +61,88 @@ function appendPageLinks(list) {
       });
    }
    
-   return container;
+  
    
 }
 
+//dynamically creating searh bar 
+const header = document.querySelector('.page-header');
+const searchDiv = document.createElement("div");
+searchDiv.className = "student-search";
+const input = document.createElement("input");
+input.placeholder = "Search for students...";
+const button = document.createElement("button");
+button.textContent = "Search";
+
+searchDiv.appendChild(input);
+searchDiv.appendChild(button);
+header.appendChild(searchDiv);
 
 
 
+//display the student that match on the page or print error message if not 
+function searchFunc (searchInput, studentsItems){
+     const query = searchInput.value.toLowerCase();
+     const searchResult = [];
+     
+     studentsItems.forEach(item =>{
+     const itemText = item.children[0].children[1].textContent.toLowerCase()
+     
+     if(isMatch(itemText,query)){
+      item.style.display = 'block';
+      searchResult.push(item);   
+   } else {
+      item.style.display = 'none';       
+   } 
+     
+   })
 
-//calling the functions 
+   //dynamically creatting div element that has the error message 
+   const messageDiv = document.createElement('div');
+   const h2 = document.createElement('h2');
+   messageDiv.appendChild(h2);
+   container.appendChild(messageDiv);
+
+   //checking the search result array and set either the alert function or pagiation according to the array length
+   if (searchResult.length == 0) {
+      div.innerHTML='';
+      messageDiv.textContent = 'no result found';
+   }
+   else {
+      appendPageLinks(searchResult);
+
+   }
+
+   }
+
+  
+
+
+
+   
+   //search function to find the matching student
+   function isMatch( itemText, query ){
+     if (query.length >= 0) {
+   return itemText.indexOf(query) >= 0;
+     }
+     return false
+     
+   }
+   
+//event Listener to the submit button
+button.addEventListener('click', () => {
+searchFunc(input, studentList);
+
+});
+ 
+//event listener to search when we start writting 
+ input.addEventListener('keyup', () => {
+   searchFunc(input, studentList);
+
+});
+
+
+
+// //calling the functions 
 showPage(studentList, 1);
 appendPageLinks(studentList);
-
-
-
